6d27bec532e607e0b0e8c2b61ffcd6ce65d48940
DreamCats <maifeng_cat@outlook.com>
2019-01-19 16:50:31 +0800

    获取真实的下载链接

--- anime.py
+++ anime.py
@@ -10,7 +10,9 @@ class Anime(object):
         # url
         self.url = 'https://wall.alphacoders.com/search.php?search={keyword}&lang=Chinese'.format(keyword=keyword)
         # base_url 
-        self.base_url = 'https://wall.alphacoders.com'
+        self.base_url = 'https://wall.alphacoders.com/'
+        # post_url 获取真实链接
+        self.post_url = 'https://wall.alphacoders.com/get_download_link.php'
         # 实例 requests.session
         self.s = requests.session()
 
@@ -28,7 +30,7 @@ class Anime(object):
                     items = doc('.boxgrid a').items()
                     for item in items:
                         url = self.base_url + item.attr('href')
-                        print(url)
+                        self.parse_url(url)
                 else:
                     print('没有相应的动漫壁纸...')
                     return None
@@ -42,6 +44,29 @@ class Anime(object):
         '''解析img_url获取下载链接
 
         '''
+        try:
+            res_url = self.s.get(url=url)
+            if res_url.status_code == 200:
+                doc = pq(res_url.text)
+                data_id = doc('.download-button').attr('data-id')
+                data_type = doc('.download-button').attr('data-type')
+                data_server = doc('.download-button').attr('data-server')
+                data_user_id = doc('.download-button').attr('data-user-id')
+                post_data = {
+                    'wallpaper_id':data_id,
+                    'type':data_type,
+                    'server':data_server,
+                    'user_id':data_user_id
+                }
+                res_url = self.s.post(url=self.post_url, data=post_data)
+                if res_url.status_code == 200:
+                    print(res_url.text)
+                else:
+                    return None
+            else:
+                return None
+        except Exception as e:
+            print('parse_url->error', e)
         pass
 
     def run(self):
